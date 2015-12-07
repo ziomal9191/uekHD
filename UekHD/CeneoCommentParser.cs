f5b6061680ba21c399d19a24f85673fb15de176b
jakubwozniczka <wozniczka.jakub@gmail.com>
2015-12-07 19:39:07 +0100

    Add db project

--- UekHD/CeneoCommentParser.cs
+++ UekHD/CeneoCommentParser.cs
@@ -1,8 +1,20 @@
 ﻿using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Data.Entity.Core.EntityClient;
 
 namespace UekHD
 {
-    
+    public class CommentDb
+    {
+        public int CommentDbID { set; get; }
+        public string Comment {set; get;}
+    }
+    public class CommentContext : DbContext
+    {
+        public DbSet<CommentDb> Comments {set; get; }
+    }
     class CeneoCommentParser : ICommentParser
     {
         public CommentList getCommentsContentFromPage(string pageContent)
@@ -28,12 +40,22 @@ namespace UekHD
                     //                                                                               
                     HtmlAgilityPack.HtmlNodeCollection bodyNodes = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"body\"]/div[2]/div/div/div[2]/div[3]/div[2]/ol/li/div/div[1]/p");// //body//div[@id='body']class=\"product - review - body\"");
                     if (bodyNodes != null)
-                        using (System.IO.StreamWriter outputFile = new System.IO.StreamWriter("./lala.txt", true, System.Text.Encoding.UTF8))
+                        using (System.IO.StreamWriter outputFile = new System.IO.StreamWriter("C:\\lala.txt", true, System.Text.Encoding.UTF8))
                         {
                             var encoding = new System.Text.UTF8Encoding();
                             foreach (HtmlAgilityPack.HtmlNode node in bodyNodes)
                             {
                                 listOfComments.Add(new CeneoProductComment(node.InnerText));
+                                using (var db = new CommentContext())
+                                {
+                                    CommentDb comment = new CommentDb { Comment= node.InnerText };
+
+                                    db.Comments.Add(comment);
+                                    db.SaveChanges();
+
+                                    //SqlConnection sc = (SqlConnection)ec.StoreConnection;
+                                    //outputFile.WriteLine(db.Database.Connection.ConnectionString);
+                                }
                                 text += node.InnerText;
                                
                             }
